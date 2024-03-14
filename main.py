2dbafdb6da3748815e2a45754e2c0a0dc38fef1d
Vlad2345553453 <130825340+Vlad2345553453@users.noreply.github.com>
2024-03-14 17:23:20 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template,  url_for, redirect, request
+import settings as stg
+import db_scripts as db
+
+app = Flask(__name__, static_folder=stg.PATH_STATIC )
+app.config["SECRET_KEY"] = stg.SECRET_KEY
+
+@app.route("/")
+@app.route("/index")
+def index():
+   return render_template("index.html")
+
+@app.route("/post/category/<category_name>", methods=["GET", "POST"])
+def post_category(category_name):
+    category_id = db.getIdByCategory(category_name)
+
+    if request.method == "GET":
+        posts = db.getPostsByCategory(category_name)
+
+    if request.method == "POST":
+        db.addPost(request.form["category_id"], request.form["post"])
+        posts = db.getPostsByCategory(category_name)
+        return redirect(f"/post/category/{category_name}")
+    
+    return render_template("post_category.html", 
+                           category_name=category_name,
+                           category_id=category_id,
+                           posts = posts) 
+
+
+@app.route("/post/view")
+def post_view():
+    return "ТУТ БУДЕ ПОСТ"
+
+@app.route("/about")
+def about():
+    user = db.getUser()
+    print(user)
+    return render_template("about.html", user=user)
+
+@app.route("/post/delete/<post_id>/<category_name>")
+def deletePost(post_id, category_name):
+    db.deletePost(post_id)
+    return redirect(f"/post/category/{category_name}")
+
+
+ 
+app.run(debug=True)
---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+PATH = os.path.dirname(__file__) + os.sep
+PATH_STATIC = PATH + "static" + os.sep
+
+SECRET_KEY = "E74572A88EB98992145667E58596A"
